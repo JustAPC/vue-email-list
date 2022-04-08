9bf29ebb99f0008e7303895ab5fcd20f24e112f3
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-08 17:51:48 +0200

    stesura completa esercizio

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+          emailList :[]
+        },  
+        
+        created() {
+            for (i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.emailList.push(response.data.response)
+                    })
+            }
+          }
+        },
+) 
+
